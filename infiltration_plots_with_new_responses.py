ece81811d4e53a275c03236f3935fa5e2866340d
Vedang Narain <vedangnarain@hotmail.com>
2023-11-03 10:23:59 -0400

    Scatter plots

--- infiltration_plots_with_new_responses.py
+++ infiltration_plots_with_new_responses.py
@@ -39,10 +39,10 @@ df_2 = df_2[df_2['BEST RESPONSE'] != 'NOT']  # remove failed classifications
 merged_df = pd.merge(df_1, df_2, on='ID', how='inner')
 #merged_df = merged_df.dropna(subset=['Response'])
 merged_df = merged_df.drop_duplicates()
+merged_df = merged_df.drop(13)  # drop extreme value
 df_NR = merged_df[merged_df['BEST RESPONSE'] == 'NR']
 df_RE = merged_df[merged_df['BEST RESPONSE'] == 'RE']
 #df_RE = df_RE.iloc[:-2, :]
-df_RE = df_RE.drop(13)  # drop extreme value
 #df_PD = merged_df[merged_df['Response'] == 'PD']
 #
 ## Plot all the patients
@@ -51,51 +51,135 @@ df_RE = df_RE.drop(13)  # drop extreme value
 #NR = savgol_filter(df_NR.mean(axis=0), window_size, poly_order)
 #RE = savgol_filter(df_RE.mean(axis=0), window_size, poly_order)
 distance = np.linspace(-480,480,25)
+width = 3
+orange = '#ff914d'
+line_styles = ['-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--', '-.', ':', '-', '--']
+threshold = 2  # Adjust this threshold as needed
+# Apply condition and keep only rows where all values are below the threshold
+df_RE_big = df_RE[df_RE.max(axis=1) >= threshold]
+df_RE_small = df_RE[df_RE.max(axis=1) <= threshold]
 
-# Plot the average cell count
-#'''
+# Plot the average cell count for three groups
+'''
 plt.figure()
 plt.xlabel('distance from tumour-stroma layer (μm)')
 plt.ylabel('[CD3+ CD8+ CD103+] per mm²')
 #    plt.plot(-distance, dataframe.mean(axis=0))  #, label=merged_df['ID'])
-plt.plot(distance, df_NR.mean(axis=0), label='NR')  #, label=merged_df['ID'])
-plt.plot(distance, df_RE.mean(axis=0), label='RE')  #, label=merged_df['ID'])
+plt.plot(distance, df_NR.mean(axis=0), label='NR (n=' + str(df_NR.shape[0]) + ')', lw=width, ls = line_styles[0])  #, label=merged_df['ID'])
+plt.plot(distance, df_RE_big.mean(axis=0), label='RE big (n=' + str(df_RE_big.shape[0]) + ')', lw=width, ls = line_styles[1])  #, label=merged_df['ID'])
+plt.plot(distance, df_RE_small.mean(axis=0), label='RE small (n=' + str(df_RE_small.shape[0]) + ')', lw=width, ls = line_styles[2])  #, label=merged_df['ID'])
 #    for patient in range(0, dataframe.shape[0]):
 #        infiltration_line = dataframe.iloc[patient, 1:-2]
 #        smoothed_values = infiltration_line.rolling(window=window_size).mean()
 #        plt.plot(distance, infiltration_line)  #, label=merged_df['ID'])
 plt.xlim(-500, 500)
-plt.ylim(0, 2.5)
+plt.ylim(0, 5)
 plt.title('Average Cell Density')
 plt.legend()
+plt.grid(True)
 plt.show()
-#'''
+'''
+
+# Plot the average cell count for three groups
+'''
+plt.figure()
+plt.ylabel('[CD3+ CD8+ CD103+] per mm²')
+dataframe = df_NR
+dataframe_values = dataframe.iloc[:, 1:-4]
+dataframe_values = dataframe_values.sum(axis=1)
+plt.bar(range(len(dataframe_values)), dataframe_values, color='r')
+tally = len(dataframe_values)
+#dataframe_categories = dataframe['BEST RESPONSE']
+dataframe = df_RE_big
+dataframe_values = dataframe.iloc[:, 1:-4]
+dataframe_values = dataframe_values.sum(axis=1)
+plt.bar(range(tally, len(dataframe_values)+tally), dataframe_values, color='g')
+tally = tally+len(dataframe_values)
+dataframe = df_RE_small
+dataframe_values = dataframe.iloc[:, 1:-4]
+dataframe_values = dataframe_values.sum(axis=1)
+plt.bar(range(tally, len(dataframe_values)+tally), dataframe_values, color='b')
+plt.title('Average Cell Density')
+#plt.legend()
+#plt.grid(True)
+#plt.xticks(range(5,9), ['NR', 'CR'], rotation=45)
+plt.tight_layout()
+#plt.subplots_adjust(wspace=0.5) 
+plt.show()
+'''
+
+# Plot the average cell count for three groups
+'''
+plt.figure(figsize=(3,4))
+plt.ylabel('[CD3+ CD8+ CD103+] per mm²')
+dataframe = merged_df
+condition_orange = dataframe['BEST RESPONSE'] == 'RE'
+condition_blue = dataframe['BEST RESPONSE'] == 'NR'
+dataframe_values = dataframe.iloc[:, 1:-4]
+dataframe_values = dataframe_values.sum(axis=1)
+dataframe_categories = dataframe['BEST RESPONSE']
+dataframe_categories.replace('NR', 'non-responsive', regex=False)
+plt.scatter(dataframe_categories[condition_orange], dataframe_values[condition_orange], color=orange, s=100, alpha=0.5)
+plt.scatter(dataframe_categories[condition_blue], dataframe_values[condition_blue], color='C0', s=100, alpha=0.5)
+#plt.scatter(dataframe_categories, dataframe_values,  marker='o', alpha=0.5, c=dataframe_values, cmap='viridis', s=100)
+plt.title('Total Patient Cell Count')
+#plt.legend()
+#plt.grid(True)
+#plt.xticks(range(5,9), ['NR', 'CR'], rotation=45)
+plt.tight_layout()
+#plt.subplots_adjust(wspace=0.5) 
+plt.show()
+'''
+
+# Plot the average cell count
+'''
+plt.figure()
+plt.xlabel('distance from tumour-stroma layer (μm)')
+plt.ylabel('[CD3+ CD8+ CD103+] per mm²')
+#    plt.plot(-distance, dataframe.mean(axis=0))  #, label=merged_df['ID'])
+plt.plot(distance, df_NR.mean(axis=0), label='NR (n=' + str(df_NR.shape[0]) + ')', lw=width, ls = line_styles[0])  #, label=merged_df['ID'])
+plt.plot(distance, df_RE.mean(axis=0), label='RE (n=' + str(df_RE.shape[0]) + ')', lw=width, ls = line_styles[1])  #, label=merged_df['ID'])
+#    for patient in range(0, dataframe.shape[0]):
+#        infiltration_line = dataframe.iloc[patient, 1:-2]
+#        smoothed_values = infiltration_line.rolling(window=window_size).mean()
+#        plt.plot(distance, infiltration_line)  #, label=merged_df['ID'])
+plt.xlim(-500, 500)
+plt.ylim(0, 3)
+plt.title('Average Cell Density (n=' + str(df_NR.shape[0]+df_RE.shape[0]) + ')')
+plt.legend()
+plt.grid(True)
+plt.show()
+'''
 
 # Plot the individual patients
 #'''
-fig, axs = plt.subplots(2, 1, figsize=(8, 12))
+fig, axs = plt.subplots(1, 2, figsize=(16, 6))
 dataframe = df_NR
-plot_title = 'Non-responsive (n=4)'
+plot_title = 'Non-responsive (n=' + str(dataframe.shape[0]) + ')'
 for patient in range(0, dataframe.shape[0]):
     infiltration_line = dataframe.iloc[patient, 1:-4]
 #    smoothed_values = infiltration_line.rolling(window=window_size).mean()
-    axs[0].plot(distance, infiltration_line)
+    axs[0].plot(distance, infiltration_line, lw=width, ls = line_styles[patient])
     axs[0].set_ylabel('[CD3+ CD8+ CD103+] per mm²')
     axs[0].set_xlabel('distance from tumour-stroma layer (μm)')
     axs[0].set_xlim(-500, 500)
-#    axs[0].set_ylim(0, 15)
+    axs[0].set_ylim(0, 12)
     axs[0].set_title(plot_title)
+    axs[0].grid(True)
+
 dataframe = df_RE
-plot_title = 'Responsive (n=13)'
+#dataframe = df_RE_small
+plot_title = 'Responsive (n=' + str(dataframe.shape[0]) + ')'
 for patient in range(0, dataframe.shape[0]):
     infiltration_line = dataframe.iloc[patient, 1:-4]
 #    smoothed_values = infiltration_line.rolling(window=window_size).mean()
-    axs[1].plot(distance, infiltration_line)
+    axs[1].plot(distance, infiltration_line, lw=width, ls = line_styles[patient])
     axs[1].set_ylabel('[CD3+ CD8+ CD103+] per mm²')
     axs[1].set_xlabel('distance from tumour-stroma layer (μm)')
     axs[1].set_xlim(-500, 500)
-#    axs[1].set_ylim(0, 15)
+    axs[1].set_ylim(0, 12)
     axs[1].set_title(plot_title)
+    axs[1].grid(True)
 
 plt.subplots_adjust(hspace=0.25)
 #plt.legend()
